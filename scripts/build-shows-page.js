1ffc592be0f21a5c655360608969c8f952838a17
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-10-12 22:10:03 -0500

    style: shows for desktop

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -32,19 +32,38 @@ let shows = [
 ];
 
 const showsSection = document.querySelector(".shows");
-const showsColumnHeaderList = document.querySelector(".shows__column-header-list");
+
+const showsHeader = document.createElement("h2");
+showsHeader.classList.add("shows__header");
+showsHeader.innerText = "Shows";
+showsSection.appendChild(showsHeader);
+
+const showsDisplay = document.createElement("div");
+showsDisplay.classList.add("shows__display");
+showsSection.appendChild(showsDisplay)
+
+const showsHeaderList = document.createElement("ul");
+showsHeaderList.classList.add("shows__header-list");
+
+const showsHeaderListItem1 = document.createElement("li");
+showsHeaderListItem1.classList.add("shows__header-list-item");
+const showsHeaderListItem2 = document.createElement("li");
+showsHeaderListItem2.classList.add("shows__header-list-item");
+const showsHeaderListItem3 = document.createElement("li");
+showsHeaderListItem3.classList.add("shows__header-list-item");
+
+showsHeaderListItem1.innerText = "DATE";
+showsHeaderList.appendChild(showsHeaderListItem1);
+showsHeaderListItem2.innerText = "VENUE";
+showsHeaderList.appendChild(showsHeaderListItem2);
+showsHeaderListItem3.innerText = "LOCATION";
+showsHeaderList.appendChild(showsHeaderListItem3);
+
+showsDisplay.appendChild(showsHeaderList);
 
 const loopAndAppendShows = (items) => {
-    const showsHeader = document.createElement("h2");
-    showsHeader.classList.add("shows__header");
-    showsHeader.innerText = "Shows";
 
-    const showsColumnHeaderList = document.createElement("ul");
-    showsColumnHeaderList.classList.add("shows__column-headers");
-    
-    items.forEach((item) => {
-    
-
+  items.forEach((item) => {
     const showsContainer = document.createElement("div");
     showsContainer.classList.add("shows__container");
 
@@ -54,12 +73,11 @@ const loopAndAppendShows = (items) => {
     const showsList = document.createElement("ul");
     showsList.classList.add("shows__list");
 
-    const createListItem = (
-      title,
-      content,
-      isBold = false,
-      // isFirst = false
-    ) => {
+    const buyTicketsButton = document.createElement("button");
+    buyTicketsButton.classList.add("shows__button");
+    buyTicketsButton.innerText = "BUY TICKETS";
+
+    const createListItem = (title, content, isBold = false) => {
       const showsListItem = document.createElement("li");
       showsListItem.classList.add("shows__list-item");
 
@@ -79,37 +97,22 @@ const loopAndAppendShows = (items) => {
       showsListItem.appendChild(showsListItemTitle);
       showsListItem.appendChild(showsListItemContent);
 
-      // if (isFirst) {
-      //   showsListItem.classList.add("shows__list-item--first");
-      // }
-
       return showsListItem;
     };
-    
 
     showsList.appendChild(createListItem("DATE", item.date, true, true));
     showsList.appendChild(createListItem("VENUE", item.venue));
     showsList.appendChild(createListItem("LOCATION", item.location));
 
-    // showsColumnHeaderList.appendChild(createListItem("DATE", " "));
-    // showsColumnHeaderList.appendChild(createListItem("VENUE", " "));
-    // showsColumnHeaderList.appendChild(createListItem("LOCATION", ""));
-
-    const buyTicketsButton = document.createElement("button");
-    buyTicketsButton.classList.add("shows__button");
-    buyTicketsButton.innerText = "BUY TICKETS";
-
     showsContainerTop.appendChild(showsList);
     showsContainerTop.appendChild(buyTicketsButton);
     showsContainer.appendChild(showsContainerTop);
 
-    // showsSection.appendChild(showsHeader);
-    showsSection.appendChild(showsColumnHeaderList);
-    showsSection.appendChild(showsContainer);
-    
+    showsDisplay.appendChild(showsContainer);
+
     const lineBreak = document.createElement("div");
     lineBreak.classList.add("shows__line-break");
-    showsSection.appendChild(lineBreak);
+    showsDisplay.appendChild(lineBreak);
   });
 };
 
